1bb4c78352bbc96f1efdab017cb63eeb9155fff1
botamochi6277 <botamochi6277@icloud.com>
2022-07-31 14:34:53 +0900

    add sine easing

--- include/easing.hpp
+++ include/easing.hpp
@@ -1,10 +1,17 @@
 #ifndef EASING_HPP
 #define EASING_HPP
 #include <Arduino.h>
+
+// https://easings.net
 namespace easing
 {
+    template <typename T>
+    T remap(T x, T in_min, T in_max, T out_min, T out_max)
+    {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
 
-    unsigned char linear(int frame, int duration = 100)
+    int linear(int frame, int duration = 255)
     {
         if (frame < 0)
         {
@@ -14,9 +21,53 @@ namespace easing
         {
             return 255;
         }
-        auto v = map(frame, 0, duration, 0, 255);
+        auto v = remap(frame, 0, duration, 0, 255);
         return v;
     }
+
+    int inOutSine(int frame, int duration = 255)
+    {
+        if (frame < 0)
+        {
+            return 0;
+        }
+        if (frame >= duration)
+        {
+            return 255;
+        }
+        float v = sin(frame / duration * PI - 0.5f * PI); // [-1,1]
+        return remap(v, -1.0f, 1.0f, 0.0f, 255.0f);
+    }
+
+    int inSine(int frame, int duration = 255)
+    {
+        if (frame < 0)
+        {
+            return 0;
+        }
+        if (frame >= duration)
+        {
+            return 255;
+        }
+        float v = sin(frame / duration * 0.5f * PI - 0.5f * PI); // [-1,0]
+
+        return remap(v, -1.0f, 0.0f, 0.0f, 255.0f);
+    }
+
+    int outSine(int frame, int duration = 255)
+    {
+        if (frame < 0)
+        {
+            return 0;
+        }
+        if (frame >= duration)
+        {
+            return 255;
+        }
+        float v = sin(frame / duration * 0.5f * PI); //[0,1]
+        return remap(v, 0.0f, 1.0f, 0.0f, 255.0f);
+    }
+
 }
 
 #endif
