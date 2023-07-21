48fe263590329aefd148a99b5430a918f826ec7c
botamochi6277 <botamochi6277@icloud.com>
2023-07-21 23:46:05 +0900

    update srv

--- include/neopixel_service.hpp
+++ include/neopixel_service.hpp
@@ -11,16 +11,6 @@ namespace ble {
 
 class NeopixelService : public BLEService {
  private:
-  BLEUnsignedLongCharacteristic timer_char_;
-  BLEUnsignedCharCharacteristic num_pixels_;
-  BLEUnsignedCharCharacteristic num_colors_;
-  BLEUnsignedIntCharacteristic color01_;
-  BLEUnsignedIntCharacteristic color02_;
-  BLEUnsignedIntCharacteristic color03_;
-  BLEUnsignedIntCharacteristic color04_;
-
-  BLEUnsignedCharCharacteristic transition_;  // dissolve, slide, etc.
-
   const uint8_t msec_format_[7] = {
       BLE_GATT_CPF_FORMAT_UINT64,
       0b11111101,                             // exp, milli, -3
@@ -38,56 +28,79 @@ class NeopixelService : public BLEService {
                                     0x00};
 
  public:
+  BLEUnsignedLongCharacteristic timer_chr;
+  BLEUnsignedCharCharacteristic num_pixels_chr;
+  BLEUnsignedCharCharacteristic num_colors_chr;
+  BLEUnsignedIntCharacteristic color01_chr;
+  BLEUnsignedIntCharacteristic color02_chr;
+  BLEUnsignedIntCharacteristic color03_chr;
+  BLEUnsignedIntCharacteristic color04_chr;
+
+  BLEUnsignedCharCharacteristic transition_chr;  // dissolve, slide, etc.
+  BLEUnsignedCharCharacteristic noise_chr;       // (acc,time)
+  BLEUnsignedCharCharacteristic blending_chr;    // gradation, cycle
+
   NeopixelService(/* args */);
   ~NeopixelService();
 };
 
 NeopixelService::NeopixelService()
     : BLEService("19B10000-E8F2-537E-4F6C-D104768A1214"),
-      timer_char_("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify),
-      num_pixels_("19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify),
-      num_colors_("19B10003-E8F2-537E-4F6C-D104768A1214",
+      timer_chr("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify),
+      num_pixels_chr("19B10002-E8F2-537E-4F6C-D104768A1214",
+                     BLERead | BLENotify),
+      num_colors_chr("19B10003-E8F2-537E-4F6C-D104768A1214",
+                     BLERead | BLEWrite | BLENotify),
+      color01_chr("19B10004-E8F2-537E-4F6C-D104768A1214",
                   BLERead | BLEWrite | BLENotify),
-      color01_("19B10004-E8F2-537E-4F6C-D104768A1214",
-               BLERead | BLEWrite | BLENotify),
-      color02_("19B10005-E8F2-537E-4F6C-D104768A1214",
-               BLERead | BLEWrite | BLENotify),
-      color03_("19B10006-E8F2-537E-4F6C-D104768A1214",
-               BLERead | BLEWrite | BLENotify),
-      color04_("19B10007-E8F2-537E-4F6C-D104768A1214",
-               BLERead | BLEWrite | BLENotify),
-      transition_("19B10008-E8F2-537E-4F6C-D104768A1214",
-                  BLERead | BLEWrite | BLENotify) {
+      color02_chr("19B10005-E8F2-537E-4F6C-D104768A1214",
+                  BLERead | BLEWrite | BLENotify),
+      color03_chr("19B10006-E8F2-537E-4F6C-D104768A1214",
+                  BLERead | BLEWrite | BLENotify),
+      color04_chr("19B10007-E8F2-537E-4F6C-D104768A1214",
+                  BLERead | BLEWrite | BLENotify),
+      blending_chr("19B10008-E8F2-537E-4F6C-D104768A1214",
+                   BLERead | BLEWrite | BLENotify),
+      transition_chr("19B10009-E8F2-537E-4F6C-D104768A1214",
+                     BLERead | BLEWrite | BLENotify),
+      noise_chr("19B10010-E8F2-537E-4F6C-D104768A1214",
+                BLERead | BLEWrite | BLENotify) {
   // add characteristics to service
-  this->addCharacteristic(this->timer_char_);
-  this->addCharacteristic(this->num_pixels_);
-  this->addCharacteristic(this->num_colors_);
-  this->addCharacteristic(this->color01_);
-  this->addCharacteristic(this->color02_);
-  this->addCharacteristic(this->color03_);
-  this->addCharacteristic(this->color04_);
-  this->addCharacteristic(this->transition_);
+  this->addCharacteristic(this->timer_chr);
+  this->addCharacteristic(this->num_pixels_chr);
+  this->addCharacteristic(this->num_colors_chr);
+  this->addCharacteristic(this->color01_chr);
+  this->addCharacteristic(this->color02_chr);
+  this->addCharacteristic(this->color03_chr);
+  this->addCharacteristic(this->color04_chr);
+  this->addCharacteristic(this->blending_chr);
+  this->addCharacteristic(this->transition_chr);
+  this->addCharacteristic(this->noise_chr);
 
   // User Description
   BLEDescriptor timer_descriptor("2901", "timer_ms");
-  this->timer_char_.addDescriptor(timer_descriptor);
+  this->timer_chr.addDescriptor(timer_descriptor);
 
   BLEDescriptor num_pixels_descriptor("2901", "#pixels");
   BLEDescriptor num_colors_descriptor("2901", "#used_colors");
-  this->num_pixels_.addDescriptor(num_pixels_descriptor);
-  this->num_colors_.addDescriptor(num_colors_descriptor);
+  this->num_pixels_chr.addDescriptor(num_pixels_descriptor);
+  this->num_colors_chr.addDescriptor(num_colors_descriptor);
 
   BLEDescriptor color01_descriptor("2901", "color01");
   BLEDescriptor color02_descriptor("2901", "color02");
   BLEDescriptor color03_descriptor("2901", "color03");
   BLEDescriptor color04_descriptor("2901", "color04");
-  this->color01_.addDescriptor(color01_descriptor);
-  this->color02_.addDescriptor(color02_descriptor);
-  this->color03_.addDescriptor(color03_descriptor);
-  this->color04_.addDescriptor(color04_descriptor);
+  this->color01_chr.addDescriptor(color01_descriptor);
+  this->color02_chr.addDescriptor(color02_descriptor);
+  this->color03_chr.addDescriptor(color03_descriptor);
+  this->color04_chr.addDescriptor(color04_descriptor);
 
-  BLEDescriptor transition_descriptor("2901", "transition");
-  this->transition_.addDescriptor(transition_descriptor);
+  BLEDescriptor blending_descriptor("2901", "color blending type");
+  this->blending_chr.addDescriptor(blending_descriptor);
+  BLEDescriptor transition_descriptor("2901", "transition type");
+  this->transition_chr.addDescriptor(transition_descriptor);
+  BLEDescriptor noise_descriptor("2901", "noise");
+  this->noise_chr.addDescriptor(noise_descriptor);
 }
 
 NeopixelService::~NeopixelService(){};
