7db1b4390d5e0aa5ec8e449b633d177f70632635
botamochi6277 <botamochi6277@icloud.com>
2023-07-22 00:12:48 +0900

    update color handling

--- include/color_utils.hpp
+++ include/color_utils.hpp
@@ -1,66 +1,69 @@
 
 #ifndef COLOR_UTILS_HPP
 #define COLOR_UTILS_HPP
-#include <Arduino.h>
 
+#include <math.h>
+// typedef unsigned char uint8_t;
+// typedef uint32_t uint32_t;
 #include "easing.hpp"
 
-void hexToRgbw(unsigned int c, unsigned char &r, unsigned char &g,
-               unsigned char &b, unsigned char &w) {
+namespace color {
+
+void hexToRgbw(uint32_t color, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &w) {
   // 0x FF 00 FF 00
-  w = (0xFF000000 & c) >> 24;
-  r = (0x00FF0000 & c) >> 16;
-  g = (0x0000FF00 & c) >> 8;
-  b = (0x000000FF & c);
+  w = (0xFF000000 & color) >> 24;
+  r = (0x00FF0000 & color) >> 16;
+  g = (0x0000FF00 & color) >> 8;
+  b = (0x000000FF & color);
 }
 
-/**
- * @brief hsv 2 hex-color
- * @param H Hue, 0--255
- * @param S Saturation, 0--255
- * @param V Value, 0--255
- */
-unsigned int hsvToHex(unsigned char h, unsigned char s, unsigned char v) {
-  unsigned char r = 0, g = 0, b = 0;
-  int hi;
-  float f;
-  unsigned char p, q, t;
+void hexToRgb(uint32_t color, uint8_t &r, uint8_t &g, uint8_t &b) {
+  // 0x FF 00 FF 00
+  r = (0x00FF0000 & color) >> 16;
+  g = (0x0000FF00 & color) >> 8;
+  b = (0x000000FF & color);
+}
 
-  // h 0--255,
-  hi = ((int)(h / 42.5f)) % 6;  // int 0--5
-  f = h / 42.5f - hi;           // 0.0--1.0
-  p = v * ((255 - s) / 255);
-  q = v * ((255 - f * s) / 255);
-  t = v * ((255 - (1.0 - f) * s) / 255);
+uint32_t hsbToHex(uint16_t hue, uint8_t saturation, uint8_t brightness) {
+  uint8_t r = 0, g = 0, b = 0;
+
+  // 10922.5 = 65535/6 , approx 10922
+  //   hi : 0--5
+  uint8_t hi = static_cast<uint16_t>(static_cast<float>(hue) / 10922.5f) % 6;
+
+  float f = hue / (10922.5f) - hi;
+  uint8_t p = brightness * (1 - saturation / 255);
+  uint8_t q = brightness * (1 - (f * saturation) / 255);
+  uint8_t t = brightness * (1 - ((1 - f) * saturation) / 255);
 
   switch (hi) {
     case 0:
-      r = v;
+      r = brightness;
       g = t;
       b = p;
       break;
     case 1:
       r = q;
-      g = v;
+      g = brightness;
       b = p;
       break;
     case 2:
       r = p;
-      g = v;
+      g = brightness;
       b = t;
       break;
     case 3:
       r = p;
       g = q;
-      b = v;
+      b = brightness;
       break;
     case 4:
       r = t;
       g = p;
-      b = v;
+      b = brightness;
       break;
     case 5:
-      r = v;
+      r = brightness;
       g = p;
       b = q;
       break;
@@ -69,14 +72,64 @@ unsigned int hsvToHex(unsigned char h, unsigned char s, unsigned char v) {
   return (r << 16 | g << 8 | b);
 }
 
-unsigned int colorEasing(unsigned int from, unsigned int to, int frame,
-                         int duration, short ease = easing::LINEAR) {
+void rgbToHsb(uint8_t r, uint8_t g, uint8_t b, uint16_t &hue,
+              uint8_t &saturation, uint8_t &brightness) {
+  float fr = r / 255.0f;
+  float fg = g / 255.0f;
+  float fb = b / 255.0f;
+
+  float mi = std::fmin(std::fmin(fr, fg), fb);
+  float ma = std::fmax(std::fmax(fr, fg), fb);
+  float delta = ma - mi;
+
+  brightness = static_cast<uint16_t>(ma * 65535);
+
+  if (std::fabs(delta) < 1e-6) {
+    // this is gray
+    saturation = 0;
+    return;
+  }
+
+  saturation = static_cast<uint8_t>(255 * (delta / ma));
+
+  float delta_r = (((ma - fr) * 0.16666666666f) + (delta * 0.5f)) / delta;
+  float delta_g = (((ma - fg) * 0.16666666666f) + (delta * 0.5f)) / delta;
+  float delta_b = (((ma - fb) * 0.16666666666f) + (delta * 0.5f)) / delta;
+
+  float f_hue = 0.0f;
+  if (std::fabs(fr - ma) < 1e-6) {
+    f_hue = delta_b - delta_g;
+  } else if (std::fabs(fg - ma) < 1e-6) {
+    f_hue = 0.3333333333f + delta_r - delta_b;
+  } else if (std::fabs(fb - ma) < 1e-6) {
+    f_hue = 0.6666666666f + delta_g - delta_r;
+  }
+
+  if (f_hue < 0.0f) {
+    f_hue += 1.0;
+  }
+  if (f_hue > 1.0f) {
+    f_hue -= 1.0;
+  }
+
+  hue = static_cast<uint8_t>(255 * f_hue);
+}
+
+void hexToHsb(uint32_t color, uint16_t &hue, uint8_t &saturation,
+              uint8_t &brightness) {
+  uint8_t r, g, b;
+  hexToRgb(color, r, g, b);
+  rgbToHsb(r, g, b, hue, saturation, brightness);
+}
+
+uint32_t rgbEasing(uint32_t from, uint32_t to, int frame, int duration,
+                   short ease = easing::LINEAR) {
   unsigned char rf, gf, bf, wf;
   hexToRgbw(from, rf, gf, bf, wf);
   unsigned char rt, gt, bt, wt;
   hexToRgbw(to, rt, gt, bt, wt);
 
-  unsigned int color;
+  uint32_t color;
   unsigned char ri, gi, bi;
 
   unsigned char w = 0;  // weight, 0--255
@@ -102,9 +155,68 @@ unsigned int colorEasing(unsigned int from, unsigned int to, int frame,
   return color;
 }
 
-bool wipeUpdate(unsigned int colors[], unsigned int from[], unsigned int to[],
-                unsigned int num_pixels, unsigned int progress,
-                bool forward = true) {
+uint32_t hsbEasing(uint32_t from, uint32_t to, uint16_t frame,
+                   uint16_t duration, uint8_t ease = 0) {
+  uint16_t h0, h1;
+  uint8_t s0, s1, b0, b1;
+
+  hexToHsb(from, h0, s0, b0);
+  hexToHsb(to, h1, s1, b1);
+
+  uint16_t w = 0;  // weight
+  switch (ease) {
+    case easing::LINEAR:
+      w = easing::linear(frame, duration);
+      break;
+    case easing::SINE:
+      w = easing::inOutSine(frame, duration);
+      break;
+
+    default:
+      break;
+  }
+
+  uint16_t h =
+      easing::remap((uint16_t)w, (uint16_t)0, (uint16_t)0xFFFF, h0, h1);
+  uint8_t s =
+      easing::remap((uint8_t)(w / 255), (uint8_t)0, (uint8_t)0xFF, s0, s1);
+  uint8_t b =
+      easing::remap((uint8_t)(w / 255), (uint8_t)0, (uint8_t)0xFF, b0, b1);
+
+  return hsbToHex(h, s, b);
+}
+void blend(uint32_t colors[], uint32_t palette[], uint8_t num_pixels,
+           uint8_t palette_size = 2, uint8_t blend_type = 0) {
+  if (palette_size == 0) {
+    return;
+  }
+
+  if (palette_size == 1) {
+    for (size_t i = 0; i < num_pixels; i++) {
+      colors[i] = palette[0];
+    }
+    return;
+  }
+
+  // blend type
+  // 0: gradient
+  // 1: cycle
+  switch (blend_type) {
+    case 0:
+      // gradient
+      for (uint8_t i = 0; i < num_pixels; i++) {
+        colors[i] = hsbEasing(palette[0], palette[1], i, num_pixels);
+      }
+
+      break;
+
+    default:
+      break;
+  }
+}
+
+bool wipeUpdate(uint32_t colors[], uint32_t from[], uint32_t to[],
+                uint32_t num_pixels, uint32_t progress, bool forward = true) {
   bool b;
   for (size_t i = 0; i < num_pixels; i++) {
     if (forward) {
@@ -123,22 +235,19 @@ bool wipeUpdate(unsigned int colors[], unsigned int from[], unsigned int to[],
   return progress >= num_pixels - 1;
 }
 
-bool dissolveUpdate(unsigned int colors[], unsigned int from[],
-                    unsigned int to[], unsigned int num_pixels,
-                    unsigned int progress, unsigned int duration) {
+bool dissolveUpdate(uint32_t colors[], uint32_t from[], uint32_t to[],
+                    uint32_t num_pixels, uint32_t progress, uint32_t duration) {
   // float w = progress / num_pixels;
-  for (size_t i = 0; i < num_pixels; i++) {
-    // unsigned unsigned int r = w * (to[i] - from[i]) + from[i];
-    colors[i] = colorEasing(from[i], to[i], progress, duration);
+  for (uint32_t i = 0; i < num_pixels; i++) {
+    colors[i] = hsbEasing(from[i], to[i], progress, duration, 0);
   }
   return progress >= duration;
 }
 
-bool slideUpdate(unsigned int colors[], unsigned int from[], unsigned int to[],
-                 unsigned int num_pixels, unsigned int progress,
-                 bool forward = true) {
-  unsigned int head;
-  unsigned int tail;
+bool slideUpdate(uint32_t colors[], uint32_t from[], uint32_t to[],
+                 uint32_t num_pixels, uint32_t progress, bool forward = true) {
+  uint32_t head;
+  uint32_t tail;
   bool b;
   for (size_t i = 0; i < num_pixels; i++) {
     head = progress - 1;
@@ -163,10 +272,9 @@ bool slideUpdate(unsigned int colors[], unsigned int from[], unsigned int to[],
   return progress >= num_pixels;
 }
 
-bool wipe_dissolve(unsigned int colors[], unsigned int from[],
-                   unsigned int to[], unsigned int num_pixels,
-                   unsigned int progress, unsigned int num_dissolve = 3,
-                   bool forward = true) {
+bool wipe_dissolve(uint32_t colors[], uint32_t from[], uint32_t to[],
+                   uint32_t num_pixels, uint32_t progress,
+                   uint32_t num_dissolve = 3, bool forward = true) {
   bool b;
   // num_seq = num_pixels + num_dissolve
   for (size_t i = 0; i < num_pixels; i++) {
@@ -190,7 +298,7 @@ bool wipe_dissolve(unsigned int colors[], unsigned int from[],
       if (w >= 255) {
         colors[i] = to[i];
       } else {
-        colors[i] = colorEasing(from[i], to[i], w, num_dissolve);
+        colors[i] = rgbEasing(from[i], to[i], w, num_dissolve);
       }
     }
   }
@@ -198,9 +306,9 @@ bool wipe_dissolve(unsigned int colors[], unsigned int from[],
   return progress >= num_pixels + num_dissolve - 1;
 }
 
-void slide_dissolve(unsigned int colors[], unsigned int from[],
-                    unsigned int to[], unsigned int num_pixels,
-                    unsigned int progress, unsigned int num_dissolve = 3) {
+void slide_dissolve(uint32_t colors[], uint32_t from[], uint32_t to[],
+                    uint32_t num_pixels, uint32_t progress,
+                    uint32_t num_dissolve = 3) {
   for (size_t i = 0; i < num_pixels; i++) {
     if (progress <= i) {
       colors[i] = from[i];
@@ -212,7 +320,7 @@ void slide_dissolve(unsigned int colors[], unsigned int from[],
       //   int w = 255 * k / num_dissolve;
 
       colors[i] =
-          colorEasing(from[i], to[num_pixels - 1], progress, num_dissolve);
+          rgbEasing(from[i], to[num_pixels - 1], progress, num_dissolve);
     }
   }
 }
@@ -225,21 +333,22 @@ void slide_dissolve(unsigned int colors[], unsigned int from[],
  * @param left
  * @param right
  */
-void gradientColor(unsigned int colors[], size_t num_pixels, unsigned int left,
-                   unsigned int right) {
+void gradientColor(uint32_t colors[], size_t num_pixels, uint32_t left,
+                   uint32_t right) {
   for (size_t i = 0; i < num_pixels; i++) {
-    colors[i] = colorEasing(left, right, i, num_pixels);
+    colors[i] = rgbEasing(left, right, i, num_pixels);
   }
 }
 
-void rainbowColor(unsigned int colors[], size_t num_pixels,
-                  unsigned char s = 255, unsigned char v = 200) {
+void rainbowColor(uint32_t colors[], size_t num_pixels, unsigned char s = 255,
+                  unsigned char v = 200) {
   unsigned char hue = 0;
 
   for (size_t i = 0; i < num_pixels; i++) {
     hue = (255 * i) / num_pixels;
-    colors[i] = hsvToHex(hue, s, v);
+    colors[i] = hsbToHex(hue, s, v);
   }
 }
+}  // namespace color
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -32,31 +32,32 @@ unsigned int progress = 0;
 bool completed = false;  // flag for color transition
 unsigned char act;
 bool backward;
-unsigned int current_colors[NUM_PIXELS];
-unsigned int start_colors[NUM_PIXELS];
-unsigned int goal_colors[NUM_PIXELS];
+
+uint32_t current_colors[NUM_PIXELS];
+uint32_t static_colors[NUM_PIXELS];
+
+uint32_t start_colors[NUM_PIXELS];
+uint32_t goal_colors[NUM_PIXELS];
+uint32_t palette[4];
 
 Adafruit_NeoPixel pixels(NUM_PIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
+uint32_t duration = 10;
 // Create a instance of class LSM6DS3
 LSM6DS3 my_imu(I2C_MODE, 0x6A);  // I2C device address 0x6A
 
-// BLE Variables
-
-BLEService led_service(
-    "19B10000-E8F2-537E-4F6C-D104768A1214");  // BLE LED Service
-
-BLEUnsignedLongCharacteristic timer_characteristic(
-    "19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-//  Characteristic User Description Descriptor
-BLEDescriptor timer_descriptor("2901", "timer_ms");
-
-// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by
-BLEUnsignedShortCharacteristic switch_characteristic(
-    "19B10002-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
-BLEDescriptor led_descriptor("2901", "action_color");
-
 void setup() {
+  // build-in leds
+#ifdef SEEED_XIAO_NRF52840_SENSE
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  // turn off the all build-in leds
+  digitalWrite(LEDR, HIGH);
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDB, HIGH);
+#endif
+  pinMode(PIXEL_PIN, OUTPUT);
+
   Serial.begin(115200);
   Serial.println("Neopixel BLE waking");
 
@@ -68,10 +69,10 @@ void setup() {
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
-
-    pinMode(LEDR, OUTPUT);
     while (1) {
+#ifdef SEEED_XIAO_NRF52840_SENSE
       digitalWrite(LEDR, !digitalRead(LEDR));
+#endif
       delay(100);
     }
   }
@@ -79,49 +80,49 @@ void setup() {
   Serial.print("Peripheral address: ");
   String ble_address = BLE.address();
   Serial.println(ble_address);
-  String local_name = "ArduinoNeopixels" + ble_address;
+  String local_name = "ArduinoNeopixels_" + ble_address;
   // set advertised local name and service UUID:
   BLE.setDeviceName(local_name.c_str());
   BLE.setLocalName("Neopixels");
-  BLE.setAdvertisedService(led_service);
+  BLE.setAdvertisedService(pixel_srv);
 
-  // add the characteristic to the service
-  led_service.addCharacteristic(switch_characteristic);
-  switch_characteristic.addDescriptor(led_descriptor);
   // add service
-  BLE.addService(led_service);
+  BLE.addService(pixel_srv);
 
   // set the initial value for the characteristic:
-  switch_characteristic.writeValue(0x0111);
-
+  pixel_srv.num_pixels_chr.writeValue(NUM_PIXELS);
+  pixel_srv.num_colors_chr.writeValue(2U);
+  pixel_srv.color01_chr.writeValue(PALETTE_INFO);
+  pixel_srv.color02_chr.writeValue(PALETTE_LIME);
   // start advertising
   BLE.advertise();
 
-  // build-in leds
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
-  // turn off the all build-in leds
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(LEDG, HIGH);
-  digitalWrite(LEDB, HIGH);
-
-  pinMode(PIXEL_PIN, OUTPUT);
   digitalWrite(PIXEL_PIN, LOW);
   pixels.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
-  pixels.show();   // Turn OFF all pixels ASAP
+  pixels.setBrightness(20);
+  pixels.show();  // Turn OFF all pixels
+
+  // assign palette
+  palette[0] = pixel_srv.color01_chr.value();
+  palette[1] = pixel_srv.color02_chr.value();
+  palette[2] = pixel_srv.color03_chr.value();
+  palette[3] = pixel_srv.color04_chr.value();
+  color::blend(goal_colors, palette, NUM_PIXELS, 2U, 0U);
 
   for (size_t i = 0; i < NUM_PIXELS; i++) {
-    start_colors[i] = current_colors[i];
+    start_colors[i] = 0;
+    current_colors[i] = 0;
+    goal_colors[i] = palette[0];
   }
-  gradientColor(goal_colors, NUM_PIXELS, PALETTE_INFO, PALETTE_LIME);
+
   unsigned int duration = 10;
+  progress = 0;
   for (size_t i = 0; i < duration; i++) {
-    dissolveUpdate(current_colors, start_colors, goal_colors, NUM_PIXELS,
-                   progress, duration);
+    // color::dissolveUpdate(current_colors, start_colors, goal_colors,
+    // NUM_PIXELS,
+    //                       progress, duration);
     for (int i = 0; i < pixels.numPixels(); i++) {
-      // uint32_t c = current_colors[i];
-      pixels.setPixelColor(i, (uint32_t)current_colors[i]);
+      pixels.setPixelColor(i, goal_colors[i]);
     }
     progress += 1;
     pixels.show();
@@ -132,127 +133,173 @@ void setup() {
 }
 
 void loop() {
+#ifdef SEEED_XIAO_NRF52840_SENSE
   digitalWrite(LEDB, !digitalRead(LEDB));  // waiting for connection
+#endif
+  BLE.poll();
+  // is base color change
+  if (pixel_srv.color01_chr.written() || pixel_srv.color02_chr.written() ||
+      pixel_srv.color03_chr.written() || pixel_srv.color04_chr.written() ||
+      pixel_srv.blending_chr.written()) {
+    // re-blend static color
+    // assign palette
+    palette[0] = pixel_srv.color01_chr.value();
+    palette[1] = pixel_srv.color02_chr.value();
+    palette[2] = pixel_srv.color03_chr.value();
+    palette[3] = pixel_srv.color04_chr.value();
 
-  BLEDevice central = BLE.central();
-
-  // if a central is connected to peripheral:
-  if (central) {
-    digitalWrite(LEDB, HIGH);
-    digitalWrite(LEDG, LOW);
-    // print the central's MAC address:
-    sprintf(message, "Connected to central: %s", central.address().c_str());
-    Serial.println(message);
-
-    // while the central is still connected to peripheral:
-    while (central.connected()) {
-      // if the remote device wrote to the characteristic,
-      // use the value to control the LED:
-      if (switch_characteristic.written()) {
-        progress = 0;
-        completed = false;
-        mode = switch_characteristic.valueLE();
-        sprintf(message, "Receive new value: 0x%04x", mode);
-        Serial.println(message);
-
-        act = (0x0f00 & mode) >> 8;
-        sprintf(message, "Action: 0x%01x", act);
-        Serial.println(message);
-
-        backward = ((0xf000 & mode) >> 12) > 0;
-
-        unsigned char color_code1 = mode & 0x0F;
-        unsigned char color_code2 = (mode & 0xF0) >> 4;
-
-        sprintf(message, "color: 0x%01x -- 0x%01x", color_code1, color_code2);
-        Serial.println(message);
-
-        gradientColor(goal_colors, NUM_PIXELS, PALETTE[color_code1],
-                      PALETTE[color_code2]);
-
-        for (size_t i = 0; i < NUM_PIXELS; i++) {
-          start_colors[i] = current_colors[i];
-        }
-      }
-
-      // led strip update
-      if (completed == false) {
-        unsigned short duration = 30;
-        u_int32_t cc = 0;
-
-        switch (act) {
-          case PRESET_ACT_WIPE: {
-            completed = wipeUpdate(current_colors, start_colors, goal_colors,
-                                   NUM_PIXELS, progress, !backward);
-            // progress ratio = progress / (NUM_PIXELS - 1)
-          } break;
-
-          case PRESET_ACT_SLIDE: {
-            completed = slideUpdate(current_colors, start_colors, goal_colors,
-                                    NUM_PIXELS, progress);
-          } break;
-
-          case PRESET_ACT_DISSOLVE: {
-            duration = 30;
-            completed =
-                dissolveUpdate(current_colors, start_colors, goal_colors,
-                               NUM_PIXELS, progress, duration);
-          } break;
-          case PRESET_ACT_ACC: {  // imu acc
-            float x = my_imu.readFloatAccelX();
-            float y = my_imu.readFloatAccelY();
-            float z = my_imu.readFloatAccelZ();
-
-            duration = 10;
-
-            unsigned int cx = 255 * abs(x / 2.0f);
-            unsigned int cy = 255 * abs(y / 2.0f);
-            unsigned int cz = 255 * abs(z / 2.0f);
-            cc = (cx << 16 | cy << 8 | cz);
-
-            if (progress < duration) {
-              gradientColor(goal_colors, NUM_PIXELS, cc, cc);
-              dissolveUpdate(current_colors, start_colors, goal_colors,
-                             NUM_PIXELS, progress, duration);
-            } else {
-              gradientColor(current_colors, NUM_PIXELS, cc, cc);
-            }
-          }
-
-          break;
-
-          case PRESET_ACT_RAINBOW: {
-            duration = 30;
-            rainbowColor(goal_colors, NUM_PIXELS);
-            completed =
-                dissolveUpdate(current_colors, start_colors, goal_colors,
-                               NUM_PIXELS, progress, duration);
-          } break;
-
-          default:
-            break;
-        }
-
-        // apply current color
-        for (int i = 0; i < pixels.numPixels(); i++) {
-          uint32_t c = current_colors[i];
-          pixels.setPixelColor(i, c);
-        }
-        pixels.show();
-
-        progress += 1;
-
-        if (completed) {
-          sprintf(message, "0x%04x completed (%04d frames)", mode, progress);
-          Serial.println(message);
-        }
-      }
-
-      delay(DELAY_LED);
+    color::blend(goal_colors, palette, NUM_PIXELS, 2U, 0U);
+    progress = 0;
+    completed = false;
+    duration = 30;
+    for (size_t i = 0; i < NUM_PIXELS; i++) {
+      start_colors[i] = current_colors[i];
     }
-    Serial.println("Disconnection");
-    digitalWrite(LEDG, HIGH);
+  }
+  if (completed == false) {
+    completed = color::wipeUpdate(current_colors, start_colors, goal_colors,
+                                  NUM_PIXELS, progress, !backward);
+    // progress ratio = progress / (NUM_PIXELS - 1)
+
+    //  apply current color
+    for (int i = 0; i < pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, current_colors[i]);
+    }
+    pixels.show();
+
+    progress += 1;
+    delay(DELAY_LED);
   }
 
-  delay(DELAY_MS);
+  // ble
+  // BLEDevice central = BLE.central();
+  // // if a central is connected to peripheral:
+  // if (central) {
+  //   digitalWrite(LEDB, HIGH);  // turn off
+  //   digitalWrite(LEDG, LOW);   // turn on
+  //   // print the central's MAC address:
+  //   sprintf(message, "Connected to central: %s", central.address().c_str());
+  //   Serial.println(message);
+
+  // }
+  // end of ble
+
+  // // if a central is connected to peripheral:
+  // if (central) {
+  //   digitalWrite(LEDB, HIGH);
+  //   digitalWrite(LEDG, LOW);
+  //   // print the central's MAC address:
+  //   sprintf(message, "Connected to central: %s", central.address().c_str());
+  //   Serial.println(message);
+
+  //   // while the central is still connected to peripheral:
+  //   while (central.connected()) {
+  //     // if the remote device wrote to the characteristic,
+  //     // use the value to control the LED:
+  //     if (switch_characteristic.written()) {
+  //       progress = 0;
+  //       completed = false;
+  //       mode = switch_characteristic.valueLE();
+  //       sprintf(message, "Receive new value: 0x%04x", mode);
+  //       Serial.println(message);
+
+  //       act = (0x0f00 & mode) >> 8;
+  //       sprintf(message, "Action: 0x%01x", act);
+  //       Serial.println(message);
+
+  //       backward = ((0xf000 & mode) >> 12) > 0;
+
+  //       unsigned char color_code1 = mode & 0x0F;
+  //       unsigned char color_code2 = (mode & 0xF0) >> 4;
+
+  //       sprintf(message, "color: 0x%01x -- 0x%01x", color_code1,
+  //       color_code2); Serial.println(message);
+
+  //       gradientColor(goal_colors, NUM_PIXELS, PALETTE[color_code1],
+  //                     PALETTE[color_code2]);
+
+  //       for (size_t i = 0; i < NUM_PIXELS; i++) {
+  //         start_colors[i] = current_colors[i];
+  //       }
+  //     }
+
+  //     // led strip update
+  //     if (completed == false) {
+  //       unsigned short duration = 30;
+  //       u_int32_t cc = 0;
+
+  //       switch (act) {
+  //         case PRESET_ACT_WIPE: {
+  //           completed = wipeUpdate(current_colors, start_colors, goal_colors,
+  //                                  NUM_PIXELS, progress, !backward);
+  //           // progress ratio = progress / (NUM_PIXELS - 1)
+  //         } break;
+
+  //         case PRESET_ACT_SLIDE: {
+  //           completed = slideUpdate(current_colors, start_colors,
+  //           goal_colors,
+  //                                   NUM_PIXELS, progress);
+  //         } break;
+
+  //         case PRESET_ACT_DISSOLVE: {
+  //           duration = 30;
+  //           completed =
+  //               dissolveUpdate(current_colors, start_colors, goal_colors,
+  //                              NUM_PIXELS, progress, duration);
+  //         } break;
+  //         case PRESET_ACT_ACC: {  // imu acc
+  //           float x = my_imu.readFloatAccelX();
+  //           float y = my_imu.readFloatAccelY();
+  //           float z = my_imu.readFloatAccelZ();
+
+  //           duration = 10;
+
+  //           unsigned int cx = 255 * abs(x / 2.0f);
+  //           unsigned int cy = 255 * abs(y / 2.0f);
+  //           unsigned int cz = 255 * abs(z / 2.0f);
+  //           cc = (cx << 16 | cy << 8 | cz);
+
+  //           if (progress < duration) {
+  //             gradientColor(goal_colors, NUM_PIXELS, cc, cc);
+  //             dissolveUpdate(current_colors, start_colors, goal_colors,
+  //                            NUM_PIXELS, progress, duration);
+  //           } else {
+  //             gradientColor(current_colors, NUM_PIXELS, cc, cc);
+  //           }
+  //         }
+
+  //         break;
+
+  //         case PRESET_ACT_RAINBOW: {
+  //           duration = 30;
+  //           rainbowColor(goal_colors, NUM_PIXELS);
+  //           completed =
+  //               dissolveUpdate(current_colors, start_colors, goal_colors,
+  //                              NUM_PIXELS, progress, duration);
+  //         } break;
+
+  //         default:
+  //           break;
+  //       }
+
+  //       // apply current color
+  //       for (int i = 0; i < pixels.numPixels(); i++) {
+  //         uint32_t c = current_colors[i];
+  //         pixels.setPixelColor(i, c);
+  //       }
+  //       pixels.show();
+
+  //       progress += 1;
+
+  //       if (completed) {
+  //         sprintf(message, "0x%04x completed (%04d frames)", mode, progress);
+  //         Serial.println(message);
+  //       }
+  //     }
+
+  //     delay(DELAY_LED);
+  //   }
+  //   Serial.println("Disconnection");
+  //   digitalWrite(LEDG, HIGH);
+  // }
 }
